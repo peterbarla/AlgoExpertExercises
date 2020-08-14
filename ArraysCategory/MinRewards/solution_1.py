6e6e927b37dcfeb5531c78a34c1a91fcb8b78b1e
peter.barla <ppepecsek@yahoo.com>
2020-08-14 23:28:50 +0300

    MinRewards solved

---
+++ ArraysCategory/MinRewards/solution_1.py
@@ -0,0 +1,17 @@
+# O(n) time | O(n) space
+def minRewards(scores):
+    rewards = [1 for i in range(len(scores))]
+
+    for i in range(1, len(scores)):
+        if scores[i] > scores[i - 1]:
+            rewards[i] = rewards[i - 1] + 1
+
+    for i in reversed(range(len(scores) - 1)):
+        if scores[i] > scores[i + 1]:
+            rewards[i] = max(rewards[i], rewards[i + 1] + 1)
+
+    return sum(rewards)
+
+arr = [0, 4, 2, 1, 3]
+
+minRewards(arr)
