92bd616ff8e8c7e2cc2ea76335f1c799e6807dc3
peter <ppepecsek@yahoo.com>
2020-09-21 20:46:20 +0300

    PalindromePartitioningMinCuts solved

---
+++ DynamicProgramming/PalindromePartitioningMinCuts/my_solution.py
@@ -0,0 +1,23 @@
+# O(n^3) time | O(n) space
+def palindromePartitioningMinCuts(string):
+    cuts = [float('inf') for i in range(len(string))]
+    palindromes = [False for i in range(len(string))]
+    cuts[0] = 1
+
+    for i in range(1, len(string)):
+        for j in range(0, i):
+            if isPalindrome(string[j: i + 1]):
+                palindromes[i] = True
+                if palindromes[j]:
+                    cuts[i] = min(cuts[i], cuts[j] + 1)
+                else:
+                    cuts[i] = min(cuts[i], cuts[j])
+            else:
+                cuts[i] = min(cuts[i], cuts[i - 1] + 1)
+    return (cuts[-1] - 1)
+
+def isPalindrome(string):
+    return string == string[::-1]
+
+string = "ababbbabbababa"
+print(palindromePartitioningMinCuts(string))
