4553e9cc1f775e7b3462d666b5abc2bacbba3593
peter <ppepecsek@yahoo.com>
2020-09-30 22:31:36 +0300

    LongestSubstringWithoutDuplication solved

---
+++ Strings/LongestSubstringWithoutDuplication/my_solution.py
@@ -0,0 +1,26 @@
+def longestSubstringWithoutDuplication(string):
+    cache = {}
+    longest_substr = []
+
+    tmp_substr = []
+
+    i = 0
+    while i < len(string):
+        letter = string[i]
+        if letter not in cache:
+            cache[letter] = i
+            tmp_substr.append(letter)
+            i += 1
+        else:
+            if len(tmp_substr) > len(longest_substr):
+                longest_substr = tmp_substr[0:]
+            tmp_substr = []
+            i = cache[letter] + 1
+            cache = {} 
+    if len(tmp_substr) > len(longest_substr):
+        longest_substr = tmp_substr[0:]
+    return ''.join(longest_substr)
+
+string = "clementisacap"
+
+print(longestSubstringWithoutDuplication(string))
