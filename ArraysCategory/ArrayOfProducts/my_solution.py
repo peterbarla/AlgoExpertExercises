d5fc366593185169e0ad8fe2739d85ef5ff6cb5c
peter <ppepecsek@yahoo.com>
2021-01-22 00:43:34 +0200

    ArrayOfProducts format fixed

--- ArraysCategory/ArrayOfProducts/my_solution.py
+++ ArraysCategory/ArrayOfProducts/my_solution.py
@@ -1,9 +1,9 @@
 # O(n^2) time | O(n) space
 def arrayOfProducts(array):
-	result = []
-	
-	for i in range(len(array)):
-		result.append(product(array[0:i] + array[i + 1: len(array)]))
+    result = []
+    
+    for i in range(len(array)):
+        result.append(product(array[0:i] + array[i + 1: len(array)]))
     return result
     
 def product(array: list) -> int:
